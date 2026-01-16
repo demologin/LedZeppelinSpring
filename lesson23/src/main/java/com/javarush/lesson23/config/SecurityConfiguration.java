8a3acffd8b5d79b12e456fc2afcabe5f3e498a91
akhmelev <akhmelev@gmail.com>
2026-01-16 20:50:02 +0300

    Lesson23 + basic+git

---
+++ lesson23/src/main/java/com/javarush/lesson23/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.javarush.lesson23.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry->
+                        authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
